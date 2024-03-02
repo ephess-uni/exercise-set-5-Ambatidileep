ef8f40500896846ffbcfb720f678987b7e4f355e
Ambatidileep <dambati@cbu.edu>
2024-03-02 12:37:26 -0600

    Update ex_5_1.py
    Completed

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -11,7 +11,7 @@ import argparse
 def main(infile):
     line_count(infile)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This program prints the number of lines in infile.")
     parser.add_argument("infile", help="Input file to count lines")
 
